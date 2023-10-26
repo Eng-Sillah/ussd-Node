7972315a7987f9bb5b79054af54c9935d066ce67
Eng-Sillah <ibrahimssillah213@gmail.com>
2023-10-26 14:17:58 -0600

    fixing the app.listening

--- index.js
+++ index.js
@@ -3,45 +3,44 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+// Set up middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Handle USSD POST request
 app.post('/ussd', (req, res) => {
     // Read the variables sent via POST from our API
-    const {
-        sessionId,
-        serviceCode,
-        phoneNumber,
-        text,
-    } = req.body;
+    const { sessionId, serviceCode, phoneNumber, text } = req.body;
 
     let response = '';
 
     if (text == '') {
-        // This is the first request. Note how we start the response with CON
+        // This is the first request. Start the response with CON
         response = `CON What would you like to check
-        1. My account
-        2. My phone number`;
-    } else if ( text == '1') {
-        // Business logic for first level response
+1. My account
+2. My phone number`;
+    } else if (text == '1') {
+        // Business logic for the first level response
         response = `CON Choose account information you want to view
-        1. Account number`;
-    } else if ( text == '2') {
-        // Business logic for first level response
-        // This is a terminal request. Note how we start the response with END
+1. Account number`;
+    } else if (text == '2') {
+        // Business logic for the first level response
+        // This is a terminal request. Start the response with END
         response = `END Your phone number is ${phoneNumber}`;
-    } else if ( text == '1*1') {
+    } else if (text == '1*1') {
         // This is a second level response where the user selected 1 in the first instance
         const accountNumber = 'ACC100101';
-        // This is a terminal request. Note how we start the response with END
+        // This is a terminal request. Start the response with END
         response = `END Your account number is ${accountNumber}`;
     }
 
     // Send the response back to the API
-    res.set('Content-Type: text/plain');
+    res.set('Content-Type', 'text/plain');
     res.send(response);
 });
 
+// Listen on the specified port
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-  });
+    console.log(`Server started on port ${PORT}`);
+});
