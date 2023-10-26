7695f085437376c105edca4db854b6f62b5d4a5a
Eng-Sillah <ibrahimssillah213@gmail.com>
2023-10-26 12:56:46 -0600

    Update -3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/ussd', (req, res) => {
+    // Read the variables sent via POST from our API
     const {
         sessionId,
         serviceCode,
@@ -15,25 +16,27 @@ app.post('/ussd', (req, res) => {
 
     let response = '';
 
-    if (text === '') {
+    if (text == '') {
+        // This is the first request. Note how we start the response with CON
         response = `CON What would you like to check
         1. My account
         2. My phone number`;
-    } else if (text === '1') {
+    } else if ( text == '1') {
+        // Business logic for first level response
         response = `CON Choose account information you want to view
         1. Account number`;
-    } else if (text === '2') {
+    } else if ( text == '2') {
+        // Business logic for first level response
+        // This is a terminal request. Note how we start the response with END
         response = `END Your phone number is ${phoneNumber}`;
-    } else if (text === '1*1') {
+    } else if ( text == '1*1') {
+        // This is a second level response where the user selected 1 in the first instance
         const accountNumber = 'ACC100101';
+        // This is a terminal request. Note how we start the response with END
         response = `END Your account number is ${accountNumber}`;
     }
 
-    res.set('Content-Type', 'text/plain');
+    // Send the response back to the API
+    res.set('Content-Type: text/plain');
     res.send(response);
-});
-
-const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-    console.log(`USSD app is running on port ${PORT}`);
 });
